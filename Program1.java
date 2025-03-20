cb3d1ba5a12fe5689b73985f3dbbac638893bf70
UEM student <uem.user@uem.com>
2025-03-20 08:25:21 +0530

    First commit

---
+++ Program1.java
@@ -0,0 +1,14 @@
+class Program
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
