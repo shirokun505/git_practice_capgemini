a70fe11fb21b8e6d9c69a2d4f77bc9443e15c87c
UEM student <uem.user@uem.com>
2025-03-20 10:16:23 +0530

    new

--- Program1.java
+++ Program1.java
@@ -1,7 +1,7 @@
 public class Program1 {
     public static void main(String[] args) {
         for(int i=1;i<10;i++)
-        System.out.println(i+"Aaa");
+        System.out.println(i+"Aaabbbb");
     }
     
 }
