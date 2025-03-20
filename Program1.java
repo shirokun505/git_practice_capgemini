5244de03dd1419ade2a9cc1e8c2c52e11a613428
UEM student <uem.user@uem.com>
2025-03-20 09:40:01 +0530

    edit

--- Program1.java
+++ Program1.java
@@ -1,14 +1,1 @@
-class Program
-{
-    public static void main(String args[])
-    {
-        int a=10443;
-        int b=20000;
-        int t=0;
-        t=a;
-        a=b;
-        b=t;
-        System.out.println(a);
-        System.out.println(b);
-    }
-}
+class Temp
