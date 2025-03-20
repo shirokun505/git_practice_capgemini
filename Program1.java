ac92053d0404b0c370d81e223862c197e25749f7
UEM student <uem.user@uem.com>
2025-03-20 10:24:28 +0530

    newww

--- Program1.java
+++ Program1.java
@@ -2,6 +2,7 @@ public class Program1 {
     public static void main(String[] args) {
         for(int i=1;i<10;i++)
         System.out.println(i+"Aaa");
+        System.out.println("Numbers");
     }
     
 }
